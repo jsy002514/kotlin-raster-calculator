17641b82a7d0d7df6008e435ea1cc4ea5e1f5d38
jsy002514 <jsy002514@naver.com>
2025-11-08 21:21:16 +0900

    feat(raster): 래스터 클래스 구현

--- src/main/kotlin/data/Raster.kt
+++ src/main/kotlin/data/Raster.kt
@@ -1,4 +1,47 @@
 package data
 
-class Raster {
+data class Raster(
+    val width: Int,
+    val height: Int,
+    val geoTransform: GeoTransform,
+    val crs: String = "EPSG:4326",
+    val digitalnumber: Array<FloatArray>,
+    val bandIndex: Int = 1
+) {
+
+    fun getValue(x: Int, y: Int): Float?{
+        if( y !in 0 until height || x !in 0 until width){
+            return null
+        }
+        return digitalnumber[y][x]
+    }
+
+    fun setValue(x: Int, y: Int, value: Float){
+        if (y in 0 until height && x in 0 until width){
+            digitalnumber[y][x] = value
+        }
+    }
+
+    fun map(other: Raster, op: (Float, Float) -> Float): Raster{
+        require(width == other.width && height == other.height){
+            "Raster 크기가 일치하지 않습니다."
+        }
+
+        val result = Array(height){y->
+            FloatArray(width){x->
+                op(this.digitalnumber[y][x], other.digitalnumber[y][x])
+            }
+        }
+
+        return Raster(width, height, geoTransform, crs, result)
+    }
+
+    fun map(op: (Float) -> Float): Raster{
+        val result = Array(height){y->
+            FloatArray(width){x->
+                op(this.digitalnumber[y][x])
+            }
+        }
+        return Raster(width,height,geoTransform,crs,result)
+    }
 }
