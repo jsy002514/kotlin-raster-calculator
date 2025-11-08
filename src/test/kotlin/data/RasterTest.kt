b0a4eff3d995d15e114ab8f1e2f4cc3c5c762dba
jsy002514 <jsy002514@naver.com>
2025-11-08 20:47:12 +0900

    feat(rastertest): 래스터 클래스 테스트 코드 작성

--- src/test/kotlin/data/RasterTest.kt
+++ src/test/kotlin/data/RasterTest.kt
@@ -1,4 +1,37 @@
 package data
 
-import kotlin.test.Test
-import kotlin.test.ass
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RasterTest {
+
+    @Test
+    fun `DN_평균이_올바르게_계산되어야_한다`(){
+        val data = arrayOf(
+            floatArrayOf(1f, 2f, 3f),
+            floatArrayOf(4f, 5f, 6f)
+        )
+
+        val raster = Raster(
+            width = 3,
+            height = 2,
+            geoTransform = GeoTransform(0.0, 1.0, 0.0, 0.0, 0.0, -1.0),
+            data = data
+        )
+
+        assertThat (raster.meanDN()).isEqualTo(3.5f)
+    }
+
+    @Test
+    fun `DN_최소와_최대가_정확해야_한다`(){
+        val data = arrayOf(
+            floatArrayOf(10f, 5f),
+            floatArrayOf(15f, 20f)
+        )
+
+        val raster = Raster(2, 2, GeoTransform(0.0,1.0,0.0,0.0,0.0,-1.0), data)
+
+        assertThat(raster.minDN()).isEqualTo(5f)
+        assertThat(raster.maxDN()).isEqualTo(20f)
+    }
+}
